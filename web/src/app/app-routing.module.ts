facacc7869e98bbc670757dbde885c2860045e5d
Scott Wittrock <scott.witt.awp@gmail.com>
2019-07-26 23:38:04 -0400

    Created landing page

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './views/landing/landing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: '',
+    component: LandingComponent
+}];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// Angular Material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
+import { LandingComponent } from './views/landing/landing.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LandingComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule, 
+        BrowserAnimationsModule, 
+        MatToolbarModule,
+        MatTabsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
