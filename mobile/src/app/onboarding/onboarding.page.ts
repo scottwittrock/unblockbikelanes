f7f01e1e388133bd0b343486ecbe45fa2d076ad6
Scott Wittrock <scott.witt.awp@gmail.com>
2019-07-27 22:11:20 -0400

    Add camera preview

--- mobile/src/app/onboarding/onboarding.page.ts
+++ mobile/src/app/onboarding/onboarding.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-onboarding',
-  templateUrl: './onboarding.page.html',
-  styleUrls: ['./onboarding.page.scss'],
+    selector: 'app-onboarding',
+    templateUrl: './onboarding.page.html',
+    styleUrls: ['./onboarding.page.scss'],
 })
 export class OnboardingPage implements OnInit {
+    slideOpts = {
+        initialSlide: 0,
+        speed: 400
+    };
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
