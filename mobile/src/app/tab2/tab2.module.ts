8bec6be21758cd7c189ba2550bfd0ae8cfd99892
Scott Wittrock <scott.witt.awp@gmail.com>
2019-07-27 23:21:14 -0400

    Added live camera to main page

--- mobile/src/app/tab2/tab2.module.ts
+++ mobile/src/app/tab2/tab2.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { CameraPreview } from '@ionic-native/camera-preview/ngx';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab2Page }])
+    RouterModule.forChild([{ path: '', component: Tab2Page }]),
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page], 
+  providers: [CameraPreview]
 })
 export class Tab2PageModule {}
--- mobile/src/app/tab2/tab2.page.ts
+++ mobile/src/app/tab2/tab2.page.ts
@@ -1,12 +1,66 @@
 import { Component } from '@angular/core';
+import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera-preview/ngx';
+
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+    selector: 'app-tab2',
+    templateUrl: 'tab2.page.html',
+    styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
 
-  constructor() {}
+    cameraPreviewOpts: CameraPreviewOptions = {
+        x: 0,
+        y: 0,
+        width: window.screen.width,
+        height: window.screen.height,
+        camera: 'rear',
+        tapPhoto: true,
+        previewDrag: true,
+        toBack: true,
+        alpha: 1
+    }
+    constructor(private cameraPreview: CameraPreview) {
+    }
+    success;
+    ngOnInit() {
+        this.cameraPreview.startCamera(this.cameraPreviewOpts).then(
+            (res) => {
+                console.log(res)
+            },
+            (err) => {
+                console.log(err)
+            });
+            let success;
+            let int = setInterval(()=>{
+                if(!this.success){
+                    clearInterval(int);
+                    this.cameraPreview.takePicture(this.pictureOpts).then(()=>{
+                        this.success = true;
+                        console.log("success ======================================================================================")
+                    }).catch(err=>{
+                        console.log("error ======================================================================================")
+                    })
+                }
+            },1000)
+    }
+
+    picture;
+    pictureOpts: CameraPreviewPictureOptions = {
+        width: 1280,
+        height: 1280,
+        quality: 85
+      }
+
+    takeSnapshot(){
+        this.cameraPreview.takePicture(this.pictureOpts).then((imageData) => {
+            this.picture = 'data:image/jpeg;base64,' + imageData;
+            console.log(this.picture);
+          }, (err) => {
+            console.log(err);
+            this.picture = 'assets/img/test.jpg';
+          });
+    }
+
 
 }
